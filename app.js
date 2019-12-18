62d77df88be345f044f580d251773991acb34725
Sergio David Ramos <sergioramosdavid028@gmail.com>
2019-12-18 10:45:55 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
+
+
+// Body Parser 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+ 
+
+// Importar Rutas
+var appRoutes = require('./routes/app')
+var usuarioRoutes = require('./routes/usuario')
+var loginRoutes = require('./routes/login')
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>{
@@ -12,12 +25,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>
 });
 
 // Rutas
-app.get('/', (req, res, next)=>{
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
 
 // Escuchar peticiones
 app.listen(3000, () => {
---
+++ models/usuario.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema;
+
+
+var rolesValido={
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol permitido'
+}
+
+var usuarioSchema = new Schema({
+    nombre: { type: String, required: [true, 'El nombre es necesario']},
+    email: { type: String, unique: true, required: [true, 'El correo es necesario']},
+    password: { type: String, required: [true, 'La contraseña es necesario']},
+    img: { type: String, required: false},
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValido}
+});
+
+usuarioSchema.plugin(uniqueValidator,{message:'{PATH} El correo debe de ser unico'})
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
